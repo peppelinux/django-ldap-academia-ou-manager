06b69f4e310dbcade26a2e10e77dcc577af7b7b4
Giuseppe <giuseppe.demarco@unical.it>
2018-12-20 13:10:23 +0100

    session multi

--- ldap_peoples/auth.py
+++ ldap_peoples/auth.py
@@ -4,8 +4,10 @@ from django.conf import settings
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.sessions.models import Session
 # from django.contrib.auth.decorators import user_passes_test
+from django.core.mail import send_mail
 from django.db import connections
 from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 
 from unical_accounts.models import User
 from . models import LdapAcademiaUser
@@ -55,6 +57,8 @@ class LdapAcademiaAuthBackend(ModelBackend):
                                        email=lu.mail[0],
                                        first_name=lu.cn,
                                        last_name=lu.sn)
+
+        
         
         # disconnect already created session, only a session per user is allowed
         # get all the active sessions
@@ -65,5 +69,19 @@ class LdapAcademiaAuthBackend(ModelBackend):
                         session.delete()
                 except (KeyError, TypeError, ValueError):
                     pass
+
+        # if access notification email is on
+        # works only if this field is active in account model (need to be customized first!)
+        if hasattr(user, 'access_notification'):
+            if user.access_notification:
+                send_mail(_('Access notification'),
+                              _('Access notification occurred at: {}'.format(timezone.localtime())),
+                              settings.DEFAULT_FROM_EMAIL,
+                              lu.mail, # it's a list :)
+                              fail_silently=True,
+                              auth_user=None,
+                              auth_password=None,
+                              connection=None,
+                              html_message=None)
         
         return user

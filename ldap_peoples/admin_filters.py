31197d3c15ae446d4ba187c2309e7decfaf0498f
Giuseppe De Marco <giuseppe.demarco@unical.it>
2020-01-28 14:07:13 +0100

    MultipleFilters on Affiliation

--- ldap_peoples/admin_filters.py
+++ ldap_peoples/admin_filters.py
@@ -18,3 +18,21 @@ class TitleListFilter(MultipleChoiceListFilter):
                 for dip in q.values_list('pk'):
                     pk_list.append(dip[0])
             return queryset.filter(pk__in=pk_list)
+
+
+class AffiliationListFilter(MultipleChoiceListFilter):
+    title = 'Affiliation'
+    parameter_name = 'eduPersonAffiliation'
+
+    def lookups(self, request, model_admin):
+        return [(k, v) for k,v in sorted(settings.AFFILIATION)]
+
+    def queryset(self, request, queryset):
+        pk_list = []
+        if request.GET.get(self.parameter_name):
+            for value in request.GET[self.parameter_name].split(','):
+                kwargs = {self.parameter_name: value}
+                q = queryset.filter(**kwargs)
+                for dip in q.values_list('pk'):
+                    pk_list.append(dip[0])
+            return queryset.filter(pk__in=pk_list)

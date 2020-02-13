8231711198a88641d1402949174fe355f1bc6daa
Giuseppe <giuseppe.demarco@unical.it>
2020-02-13 15:47:41 +0100

    auth.py changes
    removed email sent
    django username now is uid and not eppn anymore

--- ldap_peoples/auth.py
+++ ldap_peoples/auth.py
@@ -8,10 +8,8 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.sessions.models import Session
 # from django.contrib.auth.decorators import user_passes_test
-from django.core.mail import send_mail
 from django.db import connections
 from django.utils import timezone
-from django.utils.translation import ugettext_lazy as _
 
 from . models import LdapAcademiaUser
 
@@ -50,19 +48,21 @@ class LdapAcademiaAuthBackend(ModelBackend):
         if not lu.is_active():
             return None
 
-        scoped_username = '@'.join((lu.uid, settings.LDAP_BASE_DOMAIN))
+        # scoped_username = '@'.join((lu.uid, settings.LDAP_BASE_DOMAIN))
         try:
-            user = get_user_model().objects.get(username=scoped_username)
+            # user = get_user_model().objects.get(username=scoped_username)
+            user = get_user_model().objects.get(username=lu.uid)
             # update attrs:
             if user.email != lu.mail[0]:
                 user.email = lu.mail[0]
                 user.save()
         except Exception as e:
             user = get_user_model().objects.create(dn=lu.dn,
-                                                           username=scoped_username,
-                                                           email=lu.mail[0],
-                                                           first_name=lu.cn,
-                                                           last_name=lu.sn)
+                                                   #username=scoped_username,
+                                                   username = lu.uid,
+                                                   email=lu.mail[0],
+                                                   first_name=lu.cn,
+                                                   last_name=lu.sn)
 
         # disconnect already created session, only a session per user is allowed
         # get all the active sessions
@@ -73,33 +73,5 @@ class LdapAcademiaAuthBackend(ModelBackend):
                         session.delete()
                 except (KeyError, TypeError, ValueError):
                     pass
-
-        # if access notification email is on
-        # works only if this field is active in account model (need to be customized first!)
-        if hasattr(user, 'access_notification'):
-            if user.access_notification:
-                user_agent = ''
-                ipaddress = ''
-                if request:
-                    user_agent = request.META.get('HTTP_USER_AGENT', '')
-                    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR', ' , ')
-                    if x_forwarded_for:
-                        ipaddress = x_forwarded_for.split(',')[-1].strip()
-                    else:
-                        ipaddress = request.META.get('REMOTE_ADDR')
-                d = {'time': timezone.localtime(),
-                     'user': lu.uid,
-                     'hostname': settings.HOSTNAME,
-                     'user_agent': user_agent,
-                     'ipaddress': ipaddress}
-                send_mail(_('Access notification'),
-                              settings.IDENTITY_MSG_ACCESS.format(**d),
-                              settings.DEFAULT_FROM_EMAIL,
-                              lu.mail, # it's a list :)
-                              fail_silently=True,
-                              auth_user=None,
-                              auth_password=None,
-                              connection=None,
-                              html_message=None)
 
         return user

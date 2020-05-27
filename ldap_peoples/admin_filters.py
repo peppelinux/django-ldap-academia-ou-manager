b563ffd3a9a8eed9a7717fbd303fef0ebc4a1bb2
Giuseppe <giuseppe.demarco@unical.it>
2020-05-28 01:24:02 +0200

    modeladmin custom search filter builder

--- ldap_peoples/admin_filters.py
+++ ldap_peoples/admin_filters.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib import messages
 from django.conf import settings
 from django_admin_multiple_choice_list_filter.list_filters import MultipleChoiceListFilter
 
@@ -9,12 +10,18 @@ class GenericSearch(admin.SimpleListFilter):
     template = 'filters/custom_search.html'
 
     def lookups(self, request, model_admin):
-        return (('uid', 'uid'),
+        """ it can be overloaded as follows
+            return (('uid', 'uid'),
                 ('mail', 'mail'),
                 ('sn', 'sn'),
                 ('schacPersonalUniqueID','schacPersonalUniqueID'),
                 ('schacPersonalUniqueCode','schacPersonalUniqueCode'),
                 )
+        """
+        l = []
+        for i in model_admin.model._meta.fields:
+            l.append((i.name, i.name))
+        return l
 
     def queryset(self, request, queryset):
         """?custom_search=filter,mail__exact,peppelinux%40yahoo.it||
@@ -26,8 +33,21 @@ class GenericSearch(admin.SimpleListFilter):
             for se in search_list:
                 sple = se.split(',')
                 se_dict = {'{}__{}'.format(sple[0], sple[2]): sple[3]}
-                queryset = getattr(queryset, sple[1])(**se_dict)
+                try:
+                    queryset = getattr(queryset, sple[1])(**se_dict)
+                except Exception as e:
+                    messages.add_message(request, messages.ERROR,
+                                         'Search filter {} failed: {}'.format(se, e))
             return queryset
+
+
+    def choices(self, changelist):
+        for lookup, title in self.lookup_choices:
+            yield {
+                'selected': self.value() == str(lookup),
+                'query_string': changelist.get_query_string({self.parameter_name: lookup}),
+                'display': title,
+            }
 
 
 class TitleListFilter(MultipleChoiceListFilter):
